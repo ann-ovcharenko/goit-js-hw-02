c4c8a19f2e17cde6d088966e2e5f205413614e10
Anna Ovcharenko <anyutka1426@gmail.com>
2025-05-19 10:49:06 +0300

    finish tasks

--- js/task-3.js
+++ js/task-3.js
@@ -8,10 +8,21 @@
 // Якщо в рядку відсутні заборонені слова, функція повертає буль false
 
 function checkForSpam(message) {
-  const hasSpam = message.includes.toLowerCase.toUpperCase('spam');
-  if (hasSpam) {
+  const lowerCaseMessage = message.toLowerCase();
+  const includesSpam = lowerCaseMessage.includes('spam');
+  const includesSale = lowerCaseMessage.includes('sale');
+  const hasSpamandSale = includesSpam || includesSale;
+  if (hasSpamandSale) {
     return true;
   } else {
     return false;
   }
 }
+
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
